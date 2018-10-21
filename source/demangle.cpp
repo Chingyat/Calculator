5af4d4080c7cb062bdccc4d77c04d7bae365e324
chingyat <ego@lince.cat>
2018-10-21 13:09:46 +0800

    use boost demangle

--- source/demangle.cpp
+++ source/demangle.cpp
@@ -1,21 +1,10 @@
 #include "demangle.hpp"
 
-#include <cxxabi.h>
-#include <memory>
+#include <boost/core/demangle.hpp>
 
 namespace lince {
-
-struct StrDeleter {
-    void operator()(char *S) const noexcept
-    {
-        ::free(S);
-    }
-};
-
 std::string demangle(const std::string &Name)
 {
-    int status;
-    std::unique_ptr<char, StrDeleter> X(abi::__cxa_demangle(Name.c_str(), nullptr, nullptr, &status));
-    return X.get();
+    return boost::core::demangle(Name.c_str());
 }
 }
